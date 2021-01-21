66d4a4f5fb2ae29fbe234774a5e27004dd3ea459
Caroline Kershaw <CarolineKershaw@users.noreply.github.com>
2021-01-21 12:37:56 +0000

    Walking skeleton complete

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,19 +8,20 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'The Online Grocery Store';
-  users: any;
+  products: any;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.getUsers();
+    this.getProducts();
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
+  getProducts() {
+    this.http.get('https://localhost:5001/api/products').subscribe(response => {
+      this.products = response;
     }, error => {
       console.log(error);
     })
   }
+
 }
